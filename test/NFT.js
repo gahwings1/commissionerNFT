9e47a67c08fe36b1f97e621c810868ecd4f4f79a
George Hoffmann <143972351+gahwings1@users.noreply.github.com>
2025-03-24 20:25:27 -0400

    Work On Commissioner Contracts

--- test/NFT.js
+++ test/NFT.js
@@ -10,15 +10,24 @@ const ether = tokens
 describe('NFT', () => {
   const NAME = 'BSB Commissioner Token'
   const SYMBOL = 'BSBCT'
+  const COST = ether(10)
+  const MAX_SUPPLY = '10000'
+  const ALLOW_MINTING_ON = (Date.now()+120000).toString().slice(0, 10)
+  const BASE_URI = 'ipfs://Qmctoa42DZkprgEtoC5hYEZm6xU9iuHZuuK7P8PEf8Y88E/'
 
-  let nft
+  let nft, deployer, minter
 
   beforeEach(async () => {
-    const NFT = await ethers.getContractFactory('NFT')
-    nft = await NFT.deploy('BSB Commissioner Token', 'BSBCT')
+    let accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    minter = accounts[1]
   })
 
   describe('Deployment', () => {
+    beforeEach(async () => {
+      const NFT = await ethers.getContractFactory('NFT')
+      nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+    })
 
     it('has correct name', async () => {
       expect(await nft.name()).to.equal(NAME)
@@ -26,6 +35,26 @@ describe('NFT', () => {
 
     it('has correct symbol', async () => {
       expect(await nft.symbol()).to.equal(SYMBOL)
+    })
+
+    it('returns the cost to mint', async () => {
+      expect(await nft.cost()).to.equal(COST)
+    })
+
+    it('returns the maximum total supply', async () => {
+      expect(await nft.maxSupply()).to.equal(MAX_SUPPLY)
+    })
+
+    it('returns the allowed minting time', async () => {
+      expect(await nft.allowMintingOn()).to.equal(ALLOW_MINTING_ON)
+    })
+
+    it('returns the base URI', async () => {
+      expect(await nft.baseURI()).to.equal(BASE_URI)
+    })
+
+    it('returns the owner', async () => {
+      expect(await nft.owner()).to.equal(deployer.address)
     })
 
   })
